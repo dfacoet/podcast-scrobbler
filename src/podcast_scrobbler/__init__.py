caef61e804f765d603bc584895799c533c4e5713
dfacoet <11651258+dfacoet@users.noreply.github.com>
2025-12-14 00:03:54 +0000

    CLI top

--- src/podcast_scrobbler/__init__.py
+++ src/podcast_scrobbler/__init__.py
@@ -2,19 +2,19 @@ import typer
 from .authenticate import (
     get_authenticated_lastfm_network,
 )  # TODO: relative import in package
+from .top import top
 
 app = typer.Typer()
 
 
 @app.command()
-def test_authentication():
+def user():
     network = get_authenticated_lastfm_network()
     user = network.get_authenticated_user()
     print(f"Authenticated: {user}")
-    top_artists = user.get_top_artists(limit=18)
-    print("Your top artists:")
-    for k, artist in enumerate(top_artists):
-        print(f"{k + 1:>2}. {artist.item.name}: {artist.weight} scrobbles")
+
+
+app.command()(top)
 
 
 if __name__ == "__main__":
---
+++ src/podcast_scrobbler/top.py
@@ -0,0 +1,41 @@
+import typer
+from enum import Enum
+
+from pylast import TopItem, Artist, Album, Track
+
+from . import get_authenticated_lastfm_network
+
+
+class TopType(Enum):
+    ARTISTS = "artists"
+    ALBUMS = "albums"
+    TRACKS = "tracks"
+
+
+# TODO: better formatting, align number of scrobbles
+
+
+def format_top_item(i: TopItem) -> str:
+    base: str
+    match i.item:
+        case Artist(name=name):
+            base = name
+        case Album(artist=artist, title=title) | Track(artist=artist, title=title):
+            base = f"{artist} - {title}"
+    return f"{base}: {i.weight} scrobbles"
+
+
+def top(top_type: TopType, n: int = typer.Argument(20)):
+    network = get_authenticated_lastfm_network()
+    user = network.get_authenticated_user()
+    match top_type:
+        case TopType.ARTISTS:
+            method = user.get_top_artists
+        case TopType.ALBUMS:
+            method = user.get_top_albums
+        case TopType.TRACKS:
+            method = user.get_top_tracks
+    top = method(limit=n)
+    print(f"Your top {top_type.value}:")
+    for k, x in enumerate(top):
+        print(f"{k + 1:>2}. {format_top_item(x)}")
